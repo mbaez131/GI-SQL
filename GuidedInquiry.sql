9507d35dcbf0b7186d333a89548b345353865a1c
mbaez131 <71612415+mbaez131@users.noreply.github.com>
2021-02-02 22:00:46 -0500

    Add files via upload

---
+++ GuidedInquiry.sql
@@ -0,0 +1,77 @@
+-- Very Easy Challenge
+-- create table Cars (
+-- Name varchar(255),
+-- Make varchar(255),
+-- Model varchar(255),
+-- Year int
+-- );
+-- INSERT INTO Cars (Name, Make, Model, Year)
+-- VALUES ('Mustang GT', 'Ford', 'Mustang GT', '2021');
+-- INSERT INTO Cars (Name, Make, Model, Year)
+-- VALUES ('Corvette', 'Chevrolet', 'Stingray', '2021');
+-- INSERT INTO Cars (Name, Make, Model, Year)
+-- VALUES ('Hellcat', 'Dodge', 'SRT', '2021');
+-- Select * from Cars;
+
+-- Easy Challenge
+-- create table Books (
+-- Title varchar(255),
+-- PublishDate date,
+-- AuthorFirstName varchar(255),
+-- AuthorLastName varchar(255)
+-- );
+-- INSERT INTO Books (Title, PublishDate, AuthorFirstName, AuthorLastname)
+-- Values ('Breaking Dawn', '2011-11-18', 'Stephanie', 'Meyer');
+-- INSERT INTO Books (Title, PublishDate, AuthorFirstName, AuthorLastname)
+-- Values ('Twilight', '2005-10-05', 'Stephanie', 'Meyer');
+-- INSERT INTO Books (Title, PublishDate, AuthorFirstName, AuthorLastname)
+-- Values ('Eclipse', '2007-08-07', 'Stephanie', 'Meyer');
+-- INSERT INTO Books (Title, PublishDate, AuthorFirstName, AuthorLastname)
+-- Values ('Breaking Dawn Part 2', '2012-11-12', 'Stephanie', 'Meyer');
+-- INSERT INTO Books (Title, PublishDate, AuthorFirstName, AuthorLastname)
+-- Values ('New Moon', '2006-09-06', 'Stephanie', 'Meyer');
+-- INSERT INTO Books (Title, PublishDate, AuthorFirstName, AuthorLastname)
+-- Values ('Breaking Bad', '2009-11-03', 'Jon', 'Jones');
+-- INSERT INTO Books (Title, PublishDate, AuthorFirstName, AuthorLastname)
+-- Values ('Real Grit', '2005-07-29', 'Vladimir', 'Hind');
+-- DELETE FROM `Books` WHERE PublishDate IS NOT NULL order by PublishDate desc LIMIT 1;
+-- SELECT SUM(`Title`) FROM `Books`;
+
+-- create table Movies (
+-- Title varchar(255),
+-- ReleaseDate date,
+-- Rating int
+-- );
+-- INSERT INTO Movies (Title, ReleaseDate, Rating)
+-- VALUES ('Twilight', '2003-11-03', 9);
+-- INSERT INTO Movies (Title, ReleaseDate, Rating)
+-- VALUES ('Twilight2', '2004-11-03', 9);
+-- INSERT INTO Movies (Title, ReleaseDate, Rating)
+-- VALUES ('Twilight3', '2005-11-03', 9);
+-- INSERT INTO Movies (Title, ReleaseDate, Rating)
+-- VALUES ('Twilight4', '2006-11-03', 9);
+-- INSERT INTO Movies (Title, ReleaseDate, Rating)
+-- VALUES ('Twilight5', '2007-11-03', 9);
+-- INSERT INTO Movies (Title, ReleaseDate, Rating)
+-- VALUES ('Twilight6', '2008-11-03', 9);
+-- INSERT INTO Movies (Title, ReleaseDate, Rating)
+-- VALUES ('Twilight7', '2009-11-03', 9);
+-- INSERT INTO Movies (Title, ReleaseDate, Rating)
+-- VALUES ('Twilight8', '2010-11-03', 9);
+-- INSERT INTO Movies (Title, ReleaseDate, Rating)
+-- VALUES ('Twilight9', '2011-11-03', 9);
+-- INSERT INTO Movies (Title, ReleaseDate, Rating)
+-- VALUES ('Twilight10', '2012-11-03', 9);
+-- select * from Movies;
+
+
+
+
+
+
+
+
+
+
+
+
